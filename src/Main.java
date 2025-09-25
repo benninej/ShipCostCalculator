58491b91f97f70e14f6481d747973917cc8bd36b
elliebennings <benninej@mail.uc.edu>
2025-09-25 13:18:15 -0400

    Completed Program

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,32 @@ public class Main {
         double totalPrice = 0.0;
         final double SHIP_RATE = 0.02;
         double shipCost = 0.0;
+        String trash = "";
 
         System.out.println("Enter the item price");
-        itemPrice = input.nextDouble();
-
-        if (itemPrice >= 100)
+        if(input.hasNextDouble())
         {
-            shipCost = 0.0;
-            totalPrice = itemPrice;
+            itemPrice = input.nextDouble();
+            if (itemPrice >= 100)
+            {
+                shipCost = 0.0;
+                totalPrice = itemPrice;
+            }
+            else
+            {
+                shipCost = itemPrice * SHIP_RATE;
+                totalPrice = itemPrice + shipCost;
+            }
+            System.out.printf("Item price: $%.2f\n", itemPrice);
+            System.out.printf("Shipping cost: $%.2f\n", shipCost);
+            System.out.printf("Total price: $%.2f\n",  totalPrice);
         }
         else
         {
-            shipCost = itemPrice * SHIP_RATE;
-            totalPrice = itemPrice + shipCost;
+            trash = input.nextLine();
+            System.out.println("Invalid input.");
+            System.out.println("\nExiting Program. Enter valid input.");
+            System.exit(0);
         }
-        System.out.println("The shipping cost is $" + shipCost);
-        System.out.println("The total price is $" + totalPrice);
     }
 }
