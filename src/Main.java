ae98c5723ecf9375df32363b83a735ef6ac1f02e
elliebennings <benninej@mail.uc.edu>
2025-09-25 12:52:56 -0400

    Completed Program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ShippingCostCalculator {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double itemPrice = 0.0;
